a7dc141e30eac5c6c5a57297cdb7a23df2d5ba0f
Robin Strehlow <rcode@mailbox.org>
2025-03-09 11:42:32 +0100

    Format python code

--- history.py
+++ history.py
@@ -5,7 +5,8 @@ import configparser
 import os
 import logging
 
-class FirefoxHistory():
+
+class FirefoxHistory:
     def __init__(self):
         #   Results order
         self.order = None
@@ -21,15 +22,17 @@ class FirefoxHistory():
         #   Open Firefox history database
         self.conn = sqlite3.connect(temporary_history_location)
         #   External functions
-        self.conn.create_function('hostname',1,self.__getHostname)
+        self.conn.create_function("hostname", 1, self.__getHostname)
 
     def searchPlaces(self):
         #   Firefox folder path
-        firefox_path = os.path.join(os.environ['HOME'], '.mozilla/firefox/')
+        firefox_path = os.path.join(os.environ["HOME"], ".mozilla/firefox/")
         if os.path.exists(firefox_path) is False:
-            firefox_path = os.path.join(os.environ['HOME'], 'snap/firefox/common/.mozilla/firefox/')
+            firefox_path = os.path.join(
+                os.environ["HOME"], "snap/firefox/common/.mozilla/firefox/"
+            )
         #   Firefox profiles configuration file path
-        conf_path = os.path.join(firefox_path,'profiles.ini')
+        conf_path = os.path.join(firefox_path, "profiles.ini")
 
         # Debug
         logging.debug("Config path %s" % conf_path)
@@ -43,8 +46,8 @@ class FirefoxHistory():
         prof_path = profile.get("Profile0", "Path")
 
         #   Sqlite db directory path
-        sql_path = os.path.join(firefox_path,prof_path)
-        sql_path = os.path.join(sql_path,'places.sqlite')
+        sql_path = os.path.join(firefox_path, prof_path)
+        sql_path = os.path.join(sql_path, "places.sqlite")
 
         # Debug
         logging.debug("Sql path %s" % sql_path)
@@ -54,40 +57,42 @@ class FirefoxHistory():
 
         return sql_path
 
-
     #   Get hostname from url
-    def __getHostname(self,str):
-        url = str.split('/')
-        if len(url)>2:
+    def __getHostname(self, str):
+        url = str.split("/")
+        if len(url) > 2:
             return url[2]
         else:
-            return 'Unknown'
+            return "Unknown"
 
-    def search(self,query_str):
-        query = 'SELECT DISTINCT url'
-        query += ',title FROM moz_places WHERE'
+    def search(self, query_str):
+        query = "SELECT DISTINCT url"
+        query += ",title FROM moz_places WHERE"
         #   Search terms
-        terms = query_str.split(' ')
+        terms = query_str.split(" ")
         for term in terms:
-            query += ' ((url LIKE "%%%s%%") OR (title LIKE "%%%s%%")) AND' % (term,term)
+            query += ' ((url LIKE "%%%s%%") OR (title LIKE "%%%s%%")) AND' % (
+                term,
+                term,
+            )
         #   Delete last AND
         query = query[:-4]
 
-        query += ' ORDER BY '
+        query += " ORDER BY "
         #   Firefox Frecency
-        if self.order == 'frecency':
-            query += 'frecency'
+        if self.order == "frecency":
+            query += "frecency"
         #   Visit Count
-        elif self.order == 'visit':
-            query += 'visit_count'
+        elif self.order == "visit":
+            query += "visit_count"
         #   Last Visit
-        elif self.order == 'recent':
-            query += 'last_visit_date'
+        elif self.order == "recent":
+            query += "last_visit_date"
         #   Not sorted
         else:
-            query += 'url'
+            query += "url"
 
-        query += ' DESC LIMIT %d' % self.limit
+        query += " DESC LIMIT %d" % self.limit
 
         #   Query execution
         try:
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
-from ulauncher.api.shared.event import KeywordQueryEvent, SystemExitEvent,PreferencesUpdateEvent, PreferencesEvent
+from ulauncher.api.shared.event import (
+    KeywordQueryEvent,
+    SystemExitEvent,
+    PreferencesUpdateEvent,
+    PreferencesEvent,
+)
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
@@ -9,55 +14,63 @@ from history import FirefoxHistory
 import re
 import urllib.parse
 
+
 class FirefoxHistoryExtension(Extension):
     def __init__(self):
         super(FirefoxHistoryExtension, self).__init__()
         #   Firefox History Getter
         self.history = FirefoxHistory()
         #   Ulauncher Events
-        self.subscribe(KeywordQueryEvent,KeywordQueryEventListener())
-        self.subscribe(SystemExitEvent,SystemExitEventListener())
-        self.subscribe(PreferencesEvent,PreferencesEventListener())
-        self.subscribe(PreferencesUpdateEvent,PreferencesUpdateEventListener())
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(SystemExitEvent, SystemExitEventListener())
+        self.subscribe(PreferencesEvent, PreferencesEventListener())
+        self.subscribe(PreferencesUpdateEvent, PreferencesUpdateEventListener())
+
 
 class PreferencesEventListener(EventListener):
-    def on_event(self,event,extension):
+    def on_event(self, event, extension):
         #   Results Order
-        extension.history.order = event.preferences['order']
+        extension.history.order = event.preferences["order"]
         #   Results Number
         try:
-            n = int(event.preferences['limit'])
+            n = int(event.preferences["limit"])
         except:
             n = 10
         extension.history.limit = n
-        
+
+
 class PreferencesUpdateEventListener(EventListener):
-    def on_event(self,event,extension):
+    def on_event(self, event, extension):
         #   Results Order
-        if event.id == 'order':
+        if event.id == "order":
             extension.history.order = event.new_value
         #   Results Number
-        elif event.id == 'limit':
+        elif event.id == "limit":
             try:
                 n = int(event.new_value)
                 extension.history.limit = n
             except:
                 pass
 
+
 class SystemExitEventListener(EventListener):
-    def on_event(self,event,extension):
+    def on_event(self, event, extension):
         extension.history.close()
+
 
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
-        query  = event.get_argument()
+        query = event.get_argument()
         #   Blank Query
         if query == None:
-            query = ''
+            query = ""
         items = []
-        
-        #    Open website        
-        m = re.match("^(?:([a-z-A-Z]+)://)?([a-zA-Z0-9/-_]+\.[a-zA-Z0-9/-_\.]+)(?:\?(.*))?$", query)
+
+        #    Open website
+        m = re.match(
+            "^(?:([a-z-A-Z]+)://)?([a-zA-Z0-9/-_]+\.[a-zA-Z0-9/-_\.]+)(?:\?(.*))?$",
+            query,
+        )
         protocol = "https"
         url = ""
         if m:
@@ -67,25 +80,34 @@ class KeywordQueryEventListener(EventListener):
             params = m.group(3)
             encoded = f"?{urllib.parse.quote(params)}" if params else ""
             url = f"{protocol}://{base}{encoded}"
-        
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                                    name="Open URL",
-                                                    description=url,
-                                                    on_enter=OpenUrlAction(url)))
-            
+
+            items.append(
+                ExtensionResultItem(
+                    icon="images/icon.png",
+                    name="Open URL",
+                    description=url,
+                    on_enter=OpenUrlAction(url),
+                )
+            )
+
         #   Search into Firefox History
         results = extension.history.search(query)
         for link in results:
-            #   Encode 
+            #   Encode
             hostname = link[0]
             title = link[1] if link[1] else hostname
-                
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                            name=title,
-                                            description=hostname,
-                                            on_enter=OpenUrlAction(hostname)))
+
+            items.append(
+                ExtensionResultItem(
+                    icon="images/icon.png",
+                    name=title,
+                    description=hostname,
+                    on_enter=OpenUrlAction(hostname),
+                )
+            )
 
         return RenderResultListAction(items)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     FirefoxHistoryExtension().run()
